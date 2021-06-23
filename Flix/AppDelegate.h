7f3435dd8c50c1928beed26da40b05400404dc91
smwright1 <59848745+smwright1@users.noreply.github.com>
2021-06-23 12:09:31 -0400

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Storm Wright on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
